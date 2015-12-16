162736d5195a6fe95824bd9a8bae252c3a3f1e8e
TangJR <tangjr.work@gmail.com>
2015-12-16 09:47:30 +0800

    add stcardsmenuchildvc

--- Example/STCardsMenu-Swift/STCardsMenu-Swift/STCardsMenu/STCardsMenuChildVC.swift
+++ Example/STCardsMenu-Swift/STCardsMenu-Swift/STCardsMenu/STCardsMenuChildVC.swift
@@ -11,17 +11,44 @@ import UIKit
 class STCardsMenuChildVC: UIViewController {
     
     var tintColor: UIColor?
-
+    let hamburgButton = STCardsMenuHamburgButton()
+    let titleLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
     }
     
-    lazy var hamburgButton = STCardsMenuHamburgButton {
-        let hamburgButton = STCardsMenuHamburgButton()
-        
-        return hamburgButton
-    }()
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        setupHamburgButton()
+        setupTitlabel()
+    }
+}
+
+extension STCardsMenuChildVC {
+    private func setupHamburgButton() {
+        view.addSubview(hamburgButton)
+        hamburgButton.tintColor = tintColor
+        hamburgButton.translatesAutoresizingMaskIntoConstraints = false
+        let leftConstraint = NSLayoutConstraint(item: hamburgButton, attribute: .Left, relatedBy: .Equal, toItem: view, attribute: .Left, multiplier: 1.0, constant: 20.0)
+        let topConstriant = NSLayoutConstraint(item: hamburgButton, attribute: .Top, relatedBy: .Equal, toItem: view, attribute: .Top, multiplier: 1.0, constant: 20.0)
+        let widthConstraint = NSLayoutConstraint(item: hamburgButton, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 40.0)
+        let heightConstraint =  NSLayoutConstraint(item: hamburgButton, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 30.0)
+        hamburgButton.addConstraints([widthConstraint, heightConstraint])
+        view.addConstraints([leftConstraint, topConstriant])
+    }
+    
+    private func setupTitlabel() {
+        view.addSubview(titleLabel)
+        titleLabel.text = title
+        titleLabel.textColor = tintColor
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        let leftConstraint = NSLayoutConstraint(item: titleLabel, attribute: .Left, relatedBy: .Equal, toItem: view, attribute: .Left, multiplier: 1.0, constant: 20.0)
+        let topConstraint = NSLayoutConstraint(item: titleLabel, attribute: .Top, relatedBy: .Equal, toItem: view, attribute: .Top, multiplier: 1.0, constant: 20.0)
+        let heightConstraint = NSLayoutConstraint(item: titleLabel, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 30.0)
+        titleLabel.addConstraints([heightConstraint])
+        view.addConstraints([leftConstraint, topConstraint])
+    }
 }
